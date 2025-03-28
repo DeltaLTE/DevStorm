2a410d10d92b2f52b3d3812a7b3bd1afdae660ad
DeltaLTE <josemorinho153@gmail.comm>
2025-03-28 07:39:53 +0700

    commit

---
+++ app/utama/profil/page.tsx
@@ -0,0 +1,19 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import styles from '@/app/ui/home.module.css'
+import Image from 'next/image';
+
+export default function Page() {
+    return (
+    <main>
+        <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+    </main>
+    );
+}
+   
