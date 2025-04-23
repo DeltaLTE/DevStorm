e3b75ca06a16be6a7b30cca35d22f08b0b4c7dd8
DeltaLTE <josemorinho153@gmail.comm>
2025-04-23 10:28:17 +0700

    commit

--- app/home/produk/page.tsx
+++ app/home/produk/page.tsx
@@ -3,14 +3,14 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 const products = [
-  { name: 'CHICKEN PEN', price: 'Rp 216.876', rating: 4, image: '/hero-desktop.png', href: '/home/produk/description' },
-  { name: 'PERMAINAN SUSHI GO!', price: 'Rp 124.567', rating: 4, image: '/hero-desktop.png' },
-  { name: 'GAME GORILLA POUND', price: 'Rp 157.985', rating: 3, image: '/hero-desktop.png' },
-  { name: 'ABDUCTION: GAME DASAR', price: 'Rp 231.456', rating: 4, image: '/hero-desktop.png' },
-  { name: 'GAME BULUZI', price: 'Rp 109.999', rating: 4, image: '/hero-desktop.png' },
-  { name: 'GAME HITLER RAHASIA', price: 'Rp 115.678', rating: 3, image: '/hero-desktop.png' },
-  { name: 'ULTIMATE WEREWOLF: EDISI REVISI', price: 'Rp 355.764', rating: 5, image: '/hero-desktop.png' },
-  { name: 'NIGHTMARE BEFORE CHRISTMAS', price: 'Rp 452.854', rating: 4, image: '/hero-desktop.png' },
+  { name: 'CHICKEN PEN', price: 'Rp 216.876', rating: 4, image: '/xonex-toy-novelties-screaming-chicken-pen-funny-gag-gifts.jpg', href: '/home/produk/description' },
+  { name: 'PERMAINAN SUSHI GO!', price: 'Rp 124.567', rating: 4, image: '/gamewright-games-sushi-go-game-funny-gag-gifts-30383237365921.jpg' },
+  { name: 'GAME GORILLA 800 POUND', price: 'Rp 157.985', rating: 3, image: '/dolphin-hat-games-games-800-pound-gorilla-game-10-15-min-play-time-funny-gag-gifts-37463646306465.png' },
+  { name: 'ABDUCTION: GAME DASAR', price: 'Rp 231.456', rating: 4, image: '/very-special-games-games-abducktion-base-game-funny-gag-gifts-38675237077153.jpg' },
+  { name: 'GAME BUILDZI', price: 'Rp 109.999', rating: 4, image: '/tenzi-games-buildzi-game-funny-gag-gifts-21085130227873.png' },
+  { name: 'GAME HITLER RAHASIA', price: 'Rp 115.678', rating: 3, image: '/cards-against-humanity-games-secret-hitler-game-15443119440008.jpg' },
+  { name: 'ULTIMATE WEREWOLF: EDISI REVISI', price: 'Rp 355.764', rating: 5, image: '/alliance-game-distributors-games-ultimate-werewolf-revised-edition-funny-gag-gifts-17299123306657.jpg' },
+  { name: 'NIGHTMARE BEFORE CHRISTMAS', price: 'Rp 452.854', rating: 4, image: '/usaopoly-games-nightmare-before-christmas-trivial-pursuit-funny-gag-gifts-34861108592801.png' },
 ];
 
 const ProductCard = ({ name, price, rating, image, href }: { name: string; price: string; rating: number; image: string; href?: string }) => (
