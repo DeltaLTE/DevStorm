f778d80fc24ccd32e57294d11b63d4dc0f84b35e
DeltaLTE <josemorinho153@gmail.comm>
2025-04-23 09:54:36 +0700

    commit

--- app/home/contact/page.tsx
+++ app/home/contact/page.tsx
@@ -5,19 +5,25 @@ export default function Page() {
             <main className="mt-8 p-20">
                 <section>
                     <h2 className="text-xl mb-2">Jika kalian ada pertanyaan atau ingin memesan produk, silahkan hubungi kami melalui sosial media dibawah</h2>
+                    <br />
+                    <br />
+                    <br />
                 </section>
 
                 <section>
                     <h2 className="text-xl mb-2">SOCIAL MEDIA:</h2>
                     <ul className="space-y-2">
+                        <br />
                         <li className="flex items-center gap-2">
                             <Image src="/WhatsApp.png" alt="WhatsApp" width={50} height={50} className='rounded-full'/>
                             <span>081267672379</span>
                         </li>
+                        <br />
                         <li className="flex items-center gap-2">
                             <Image src="/Instagram.png" alt="Instagram" width={50} height={50} className='rounded-full'/>
                             <span>LOLMARTIDN</span>
                         </li>
+                        <br />
                         <li className="flex items-center gap-2">
                             <Image src="/tiktok.png" alt="TikTok" width={50} height={50} className='rounded-full w-10 h-10'/>
                             <span>@LOLMARTIDN_</span>
