8fe1e91c44140d2a66b4651c48d37904cb172460
DeltaLTE <josemorinho153@gmail.com>
2025-06-11 22:00:15 +0700

    commit

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { writeFile } from 'fs/promises';
 import path from 'path';
-import { PrismaClient } from '@/generated/prisma';
+import { PrismaClient, Prisma } from '@/generated/prisma';
 import { Produk, Transaksi } from "@/app/lib/definitions"
 
 const prisma = new PrismaClient();
 
 export async function fetchProdukPrisma(search?: string, skip: number = 0, take: number = 8) {
   try {
-    const where = search
+    const where: Prisma.produkWhereInput = search
       ? {
           nama_produk: {
             contains: search,
-            mode: 'insensitive',
+            mode: 'insensitive' as const,
           },
         }
       : {};
