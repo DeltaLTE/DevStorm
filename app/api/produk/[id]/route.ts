66d5b91390c66d57a80668cb374986c59ab1e065
DeltaLTE <josemorinho153@gmail.com>
2025-06-11 21:25:46 +0700

    commit

--- app/api/produk/[id]/route.ts
+++ app/api/produk/[id]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
 export async function GET(
-  request: NextRequest,
+  res: NextRequest,
   { params }: { params: Promise <{ id: string }> }
 ) {
   try {
